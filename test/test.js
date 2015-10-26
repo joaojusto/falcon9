493b2e17624b2ae357de027253493fdebbfd3904
João Justo <jpjustonunes@hotmail.com>
2015-10-26 11:43:00 +0000

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+let assert = require('assert');
+
+describe('Sample: ', () => {
+  it('Should return 4', done => {
+    assert.equal(2 + 2, 4, 'Two plus two should be equal to 4');
+    done();
+  });
+});
