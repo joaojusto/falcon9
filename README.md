559b4f2aae8ba1b8df46b31703c6dc8f94034bff
João Justo <jpjustonunes@gmail.com>
2015-11-20 12:52:36 +0000

    WIP

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/DCKT/express-boilerplate
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
+let pg = require('pg');
 let express = require('express');
 let settings = require('./settings');
+
 let app = express();
+let dbClient = new pg.Client(settings.pg.connectionString);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -9,4 +12,5 @@ app.get('/', function (req, res) {
 
 let server = app.listen(settings.port, () => {
   console.log(`Server listening on port ${server.address().port}`);
+  console.log(dbClient.connectionParameters);
 });
